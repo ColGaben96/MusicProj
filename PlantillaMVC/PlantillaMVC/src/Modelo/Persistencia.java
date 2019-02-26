a91d7d0369e557b3c72a20ba09225f8e981711eb
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2019-02-26 09:41:53 -0500

    First Commit
    Uploaded to git

---
+++ PlantillaMVC/PlantillaMVC/src/Modelo/Persistencia.java
@@ -0,0 +1,18 @@
+package Modelo;
+
+//Representa los datos de persistencia que van a ser utilizados por el mundo y/o controlador
+//es opcional: ser puede o no tener datos.
+
+public class Persistencia 
+{
+	protected String[] phrase1 = {"Mami", "Bebe", "Baby", "Beba", "Preciosa", "Mi amor"};
+	protected String[] phrase2 = {"Te quiero", "Te deseo", "Te voy a"};
+	protected String[] phrase3 = {"dar", "ver", "cojer", "tocar", "martillar"};
+	protected String[] phrase4 = {"como a"};
+	protected String[] phrase5 = {"rata en balde", "violin prestado", "boton de maquinita", "Siria", "moto que no prende", "Israel a Palestina", "zancudo en cortina", "alpinito sin cuchara", "pega de olla", "paisa al polvo", "costeño a burra"};
+	protected String[] phrase6 = {"y te dare", "y te llevare", "y te pondre"};
+	public Persistencia() {
+		
+	}
+
+}
